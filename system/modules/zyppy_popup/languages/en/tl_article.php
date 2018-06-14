8cb9b20b1f33be434a7dc02b37873bf2718c8f38
asconsulting <andrew@andrewstevens.consulting>
2018-06-14 16:03:30 -0400

    Languages

---
+++ system/modules/zyppy_popup/languages/en/tl_article.php
@@ -0,0 +1,29 @@
+<?php
+ 
+/**
+ * Zyppy Popup
+ *
+ * Copyright (C) 2018 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/zyppy_popup
+ * @link       https://andrewstevens.consulting
+ */
+
+
+/**
+ * Fields
+ */
+
+$GLOBALS['TL_LANG']['tl_article']['popup'] 					= array("Popup", "Is this article a popup");
+$GLOBALS['TL_LANG']['tl_article']['popupDelay'] 			= array("Delay", "Delay in milliseconds before popup is shown.");
+$GLOBALS['TL_LANG']['tl_article']['popupReshowDelay'] 		= array("Reshow Delay", "Delay in minutes before reshowing a popup.");
+$GLOBALS['TL_LANG']['tl_article']['popupScrollTrigger'] 	= array("Scroll Trigger", "Trigger popup after scrolling x pixels.");
+$GLOBALS['TL_LANG']['tl_article']['popupFadeDuration'] 		= array("Fade Duration", "Duration of fadeout effect in milliseconds.");
+$GLOBALS['TL_LANG']['tl_article']['popupTrigger'] 			= array("Trigger", "CSS selector that will trigger the popup.");
+$GLOBALS['TL_LANG']['tl_article']['popupUuid'] 				= array("Unique ID", "Auto generated unique ID for popup.");
+$GLOBALS['TL_LANG']['tl_article']['popupAddClose'] 			= array("Add Close Button", "Add a close button to the popup.");
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_article']['popup_legend'] 			= 'Popup Settings';
---
+++ system/modules/zyppy_popup/languages/en/tl_module.php
@@ -0,0 +1,29 @@
+<?php
+ 
+/**
+ * Zyppy Popup
+ *
+ * Copyright (C) 2018 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/zyppy_popup
+ * @link       https://andrewstevens.consulting
+ */
+
+
+/**
+ * Fields
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['popup'] 					= array("Popup", "Is this module a popup");
+$GLOBALS['TL_LANG']['tl_module']['popupDelay'] 				= array("Delay", "Delay in milliseconds before popup is shown.");
+$GLOBALS['TL_LANG']['tl_module']['popupReshowDelay'] 		= array("Reshow Delay", "Delay in minutes before reshowing a popup.");
+$GLOBALS['TL_LANG']['tl_module']['popupScrollTrigger'] 		= array("Scroll Trigger", "Trigger popup after scrolling x pixels.");
+$GLOBALS['TL_LANG']['tl_module']['popupFadeDuration'] 		= array("Fade Duration", "Duration of fadeout effect in milliseconds.");
+$GLOBALS['TL_LANG']['tl_module']['popupTrigger'] 			= array("Trigger", "CSS selector that will trigger the popup.");
+$GLOBALS['TL_LANG']['tl_module']['popupUuid'] 				= array("Unique ID", "Auto generated unique ID for popup.");
+$GLOBALS['TL_LANG']['tl_module']['popupAddClose'] 			= array("Add Close Button", "Add a close button to the popup.");
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_module']['popup_legend'] 			= 'Popup Settings';
