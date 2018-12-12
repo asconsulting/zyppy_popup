5c51a9a52ffd043979a2e2c1a51c7ff0619c0a1c
asconsulting <andrew@andrewstevens.consulting>
2018-12-12 16:42:51 -0500

    Remove CSS Reference

--- system/modules/zyppy_popup/config/config.php
+++ system/modules/zyppy_popup/config/config.php
@@ -20,10 +20,3 @@ $GLOBALS['TL_PTY']['regular'] = 'Asc\Frontend\PagePopup';
  * Widgets
  */ 
 $GLOBALS['BE_FFL']['moduleWizard'] = 'Asc\Widget\PopupModuleWizard';
-
-/**
- * Styles
- */
- if (version_compare(VERSION, '4.4', '>=')) {
-	$GLOBALS['TL_CSS'][] = 'system/modules/zyppy_popup/assets/css/backend-contao4.css|static';
-}
