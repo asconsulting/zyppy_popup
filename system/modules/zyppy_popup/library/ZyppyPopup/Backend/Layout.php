3019b23d17ef9a1adeffd1740d2f7052a233f133
Andrew Stevens <andrew@andrewstevens.consulting>
2022-03-16 13:27:09 -0400

    Update Layout.php

--- system/modules/zyppy_popup/library/ZyppyPopup/Backend/Layout.php
+++ system/modules/zyppy_popup/library/ZyppyPopup/Backend/Layout.php
@@ -40,7 +40,11 @@ class Layout extends Contao_Backend
 					}
 				}
 				if ($boolInject) {
-					$arrSections[] = array('title'=>'Pop-up', 'id'=>'popup','template'=>'block_section','position'=>'bottom');
+					if (count($arrSections) == 1 && $arrSections[0]['id'] == '') {
+						$arrSections[0] = array('title'=>'Pop-up', 'id'=>'popup','template'=>'block_section','position'=>'bottom');
+					} else {
+						$arrSections[] = array('title'=>'Pop-up', 'id'=>'popup','template'=>'block_section','position'=>'bottom');
+					}
 					Database::getInstance()->prepare('UPDATE tl_layout SET sections=? WHERE id=?')->execute(serialize($arrSections), $objDatabase->id);
 				}
 			}
